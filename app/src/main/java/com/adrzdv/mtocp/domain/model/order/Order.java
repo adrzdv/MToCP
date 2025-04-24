b638ee1bb27cf49a6976e65feb74c72d15009eeb
Anton Drozdov <anton.drozdov2015@yandex.ru>
2025-04-24 14:38:33 +0300

    init commit

---
+++ app/src/main/java/com/adrzdv/mtocp/domain/model/order/Order.java
@@ -0,0 +1,11 @@
+package com.adrzdv.mtocp.domain.model.order;
+
+import java.time.LocalDateTime;
+
+public abstract class Order {
+    private String number;
+    private LocalDateTime revisionDateStart;
+    private LocalDateTime getRevisionDateEnd;
+
+
+}
