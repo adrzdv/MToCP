102d5821f4486945fa7679c87f23ee66f0c91dfc
Anton Drozdov <anton.drozdov2015@yandex.ru>
2025-05-14 15:42:14 +0300

    added start menu activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 28
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0"
+        versionName = "0.1.0-beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
