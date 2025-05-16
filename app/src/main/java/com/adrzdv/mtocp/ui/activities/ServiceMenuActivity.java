1d56f9e58fba57cad71e843964f4a2e00e8eef18
Anton Drozdov <anton.drozdov2015@yandex.ru>
2025-05-16 18:29:20 +0300

    added directory handler

--- app/src/main/java/com/adrzdv/mtocp/ui/activities/ServiceMenuActivity.java
+++ app/src/main/java/com/adrzdv/mtocp/ui/activities/ServiceMenuActivity.java
@@ -1,6 +1,7 @@
 package com.adrzdv.mtocp.ui.activities;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,19 +9,43 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.adrzdv.mtocp.App;
 import com.adrzdv.mtocp.R;
+import com.adrzdv.mtocp.databinding.ActivityServiceMenuBinding;
+import com.adrzdv.mtocp.databinding.ActivityStartMenuBinding;
+import com.adrzdv.mtocp.util.DirectoryHandler;
 
-public class ServiceMenuActivity extends AppCompatActivity {
+public class ServiceMenuActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ActivityServiceMenuBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_service_menu);
+        binding = ActivityServiceMenuBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        binding.cleanDirsButton.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+        if (v.getId() == R.id.clean_dirs_button) {
+            if (DirectoryHandler.cleanDirectories()) {
+                App.showToast(this, "Директории очищены");
+            } else {
+                App.showToast(this, "Ошибка очистки директорий");
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/adrzdv/mtocp/util/DirectoryHandler.java
@@ -0,0 +1,52 @@
+package com.adrzdv.mtocp.util;
+
+import android.os.Environment;
+
+import com.adrzdv.mtocp.App;
+
+import java.io.File;
+
+public class DirectoryHandler {
+    public static final String MEDIA_DIRECTORY = Environment
+            .getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS) +
+            "/mtocp/media";
+
+    public static final String WORK_DIRECTORY = Environment
+            .getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS) +
+            "/mtocp";
+
+    public static final String ZIP_DIRECTORY = Environment
+            .getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS) +
+            "/mtocp/zip";
+    public static final String EXPORT_DIRECTORY = Environment
+            .getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS) +
+            "/mtocp/export";
+
+    public static boolean cleanDirectories() {
+
+        boolean res;
+        File dir = new File(WORK_DIRECTORY);
+
+        if (dir.exists()) {
+            res = delRecursFile(dir);
+            new File(MEDIA_DIRECTORY).mkdirs();
+            new File(ZIP_DIRECTORY).mkdirs();
+            new File(EXPORT_DIRECTORY).mkdirs();
+        } else {
+            res = false;
+        }
+
+        new File(WORK_DIRECTORY).mkdir();
+        return res;
+
+    }
+
+    private static boolean delRecursFile(File file) {
+        if (file.isDirectory()) {
+            for (File child : file.listFiles()) {
+                delRecursFile(child);
+            }
+        }
+        return file.delete();
+    }
+}
