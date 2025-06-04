5cb3637ef8ef5e4a4a04fd29f19866e4f1ac1a83
Anton Drozdov <anton.drozdov2015@yandex.ru>
2025-06-04 13:02:46 +0300

    added manual for developed part

--- app/src/main/java/com/adrzdv/mtocp/ui/screen/HelpScreen.kt
+++ app/src/main/java/com/adrzdv/mtocp/ui/screen/HelpScreen.kt
@@ -2,13 +2,31 @@ package com.adrzdv.mtocp.ui.screen
 
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.adrzdv.mtocp.R
+import com.adrzdv.mtocp.ui.theme.CustomTypography
 
 @Composable
 fun HelpScreen() {
+
     AndroidView(
         modifier = Modifier.fillMaxSize(),
         factory = { context ->
@@ -18,4 +36,5 @@ fun HelpScreen() {
             }
         }
     )
+
 }
