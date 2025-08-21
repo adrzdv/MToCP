5640371e58a12ccfb93bbe1f8976df60e7cb8a5c
Anton Drozdov <anton.drozdov2015@yandex.ru>
2025-08-21 15:30:21 +0300

    m3 expressive added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,6 +75,9 @@ dependencies {
     implementation("androidx.activity:activity-compose")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.material3:material3")
+
+    implementation("androidx.compose.material3:material3:1.4.0-alpha14")
+
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.activity:activity-compose:1.10.1")
 
