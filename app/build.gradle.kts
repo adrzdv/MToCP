120a96d06a8972f22bcce862eb517744f7fdef4c
Anton Drozdov <anton.drozdov2015@yandex.ru>
2025-09-05 10:05:33 +0300

    version upgraded

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 30                                                 //Android 11+
         targetSdk = 35
         versionCode = 1
-        versionName = "0.9.2-beta-hf-04092025"
+        versionName = "0.9.2-beta-hf-05092025"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
