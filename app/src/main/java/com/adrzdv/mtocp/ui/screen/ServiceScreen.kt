af7826ebcdd66c7e3c4f6378eea8e3f695f0c27c
Anton Drozdov <anton.drozdov2015@yandex.ru>
2025-09-21 13:00:26 +0300

    unused imports deleted

--- app/src/main/java/com/adrzdv/mtocp/ui/screen/ServiceScreen.kt
+++ app/src/main/java/com/adrzdv/mtocp/ui/screen/ServiceScreen.kt
@@ -1,22 +1,12 @@
 package com.adrzdv.mtocp.ui.screen
 
 import android.content.Context
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
@@ -27,7 +17,6 @@ import androidx.compose.material3.TopAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -36,7 +25,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.adrzdv.mtocp.MessageCodes
 import com.adrzdv.mtocp.R
-import com.adrzdv.mtocp.ui.component.buttons.MenuButton
 import com.adrzdv.mtocp.ui.component.newelements.Divider
 import com.adrzdv.mtocp.ui.component.newelements.SettingsRow
 import com.adrzdv.mtocp.ui.component.newelements.SwitchRow
@@ -57,7 +45,7 @@ fun ServiceScreen(
     val snackbarHostState = remember { SnackbarHostState() }
     val coroutineScope = rememberCoroutineScope()
     val context = LocalContext.current
-    var appVersion =
+    val appVersion =
         context.packageManager.getPackageInfo(context.packageName, 0)?.versionName ?: "unknown"
     val prefs = context.getSharedPreferences("prefs", Context.MODE_PRIVATE)
     val username = prefs.getString("username", "null")
