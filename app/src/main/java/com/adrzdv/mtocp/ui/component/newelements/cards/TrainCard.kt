7168c7fa53e28a5acb275e4caee19cfed98d9106
Anton Drozdov <anton.drozdov2015@yandex.ru>
2025-10-28 16:52:09 +0300

    unused imports deleted

--- app/src/main/java/com/adrzdv/mtocp/ui/component/newelements/cards/TrainCard.kt
+++ app/src/main/java/com/adrzdv/mtocp/ui/component/newelements/cards/TrainCard.kt
@@ -18,8 +18,6 @@ import androidx.compose.material3.TooltipBox
 import androidx.compose.material3.TooltipDefaults
 import androidx.compose.material3.rememberTooltipState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
