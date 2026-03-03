5f3772ed2fda0bce31414227d9dd56e90852d681
Anton Drozdov <anton.drozdov2015@yandex.ru>
2026-03-03 15:33:23 +0300

    unused imports removed

--- app/src/main/java/com/adrzdv/mtocp/domain/repository/old/TempParamRepository.java
+++ app/src/main/java/com/adrzdv/mtocp/domain/repository/old/TempParamRepository.java
@@ -1,7 +1,6 @@
 package com.adrzdv.mtocp.domain.repository.old;
 
 import com.adrzdv.mtocp.data.db.entity.TempParametersEntity;
-import com.adrzdv.mtocp.domain.repository.BaseAppRepository;
 
 import java.util.List;
 
