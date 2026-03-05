f4fd354b9e21bcf554672eb78082702ad740c13e
Anton Drozdov <anton.drozdov2015@yandex.ru>
2026-03-05 14:47:52 +0300

    depot repository added

---
+++ app/src/main/java/com/adrzdv/mtocp/data/repository/DepotRepositoryImpl.kt
@@ -0,0 +1,25 @@
+package com.adrzdv.mtocp.data.repository
+
+import com.adrzdv.mtocp.data.db.dao.DepotDao
+import com.adrzdv.mtocp.data.db.pojo.DepotWithBranch
+import com.adrzdv.mtocp.domain.repository.DepotRepository
+
+class DepotRepositoryImpl(
+    val dao: DepotDao
+) : DepotRepository {
+
+    override suspend fun getAll(): List<DepotWithBranch> {
+        return dao.depots
+    }
+
+    override suspend fun getEntity(id: Int): DepotWithBranch {
+        return dao.getDepotById(id)
+    }
+
+    override suspend fun saveAll(entities: List<DepotWithBranch>) {
+        for (entity in entities) {
+            dao.insertDepotWithBranch(entity.depot, entity.branch)
+        }
+    }
+
+}
---
+++ app/src/main/java/com/adrzdv/mtocp/domain/repository/DepotRepository.kt
@@ -0,0 +1,7 @@
+package com.adrzdv.mtocp.domain.repository
+
+import com.adrzdv.mtocp.data.db.pojo.DepotWithBranch
+
+interface DepotRepository : BaseAppRepository<DepotWithBranch, Int> {
+
+}
