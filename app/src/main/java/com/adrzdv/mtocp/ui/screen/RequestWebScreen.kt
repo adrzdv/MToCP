dfd77f03f5e8c608719fb03442e3b754579a18b8
Anton Drozdov <anton.drozdov2015@yandex.ru>
2026-03-16 16:48:51 +0300

    RequestWebScreen updated: removed unused imports

--- app/src/main/java/com/adrzdv/mtocp/ui/screen/RequestWebScreen.kt
+++ app/src/main/java/com/adrzdv/mtocp/ui/screen/RequestWebScreen.kt
@@ -12,8 +12,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
