6519d9c83da019cb81aa91f87759847f7f3a5dca
Anton Drozdov <anton.drozdov2015@yandex.ru>
2026-04-01 16:15:38 +0300

    versionName updated to 0.9.8-beta-hf-01042026-2

--- app/src/main/java/com/adrzdv/mtocp/data/api/DocRequestApi.kt
+++ app/src/main/java/com/adrzdv/mtocp/data/api/DocRequestApi.kt
@@ -4,6 +4,7 @@ import com.adrzdv.mtocp.data.model.LogEntry
 import com.adrzdv.mtocp.data.model.NameRequest
 import com.adrzdv.mtocp.data.model.NumberResponse
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface DocRequestApi {
@@ -11,8 +12,6 @@ interface DocRequestApi {
     @POST("getnumber")
     suspend fun getNumber(@Body request: NameRequest): NumberResponse
 
-    @POST("gottennumbers")
-    suspend fun getLastLogs(
-        @Body request: Map<String, String> = emptyMap()
-    ): List<LogEntry>
+    @GET("gottennumbers")
+    suspend fun getLastLogs(): List<LogEntry>
 }
